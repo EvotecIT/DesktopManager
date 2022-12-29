0d93ceca0eb4923da81f295ce545364272d8f475
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-29 19:39:59 +0100

    Add example

---
+++ Examples/PlayWithWallpapers.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module .\DesktopManager.psd1 -Force
+
+Get-DesktopMonitors | Format-Table
+
+Get-DesktopWallpaper -Index 0 
+
+Set-DesktopWallpaper -Index 1 -WallpaperPath "C:\Support\GitHub\ImagePlayground\Sources\ImagePlayground.Examples\bin\Debug\net7.0\Images\KulekWSluchawkach.jpg" -Position Fit
+Set-DesktopWallpaper -Index 0 -WallpaperPath "C:\Users\przemyslaw.klys\Downloads\IMG_4820.jpg"
