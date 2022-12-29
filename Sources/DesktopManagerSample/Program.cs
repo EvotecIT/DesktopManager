8c6cc0fd5194136d8c4c9084d24d754a88161c1e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-29 19:41:08 +0100

    add example in c#

---
+++ Sources/DesktopManagerSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using Monitors = DesktopManager.Monitors;
+
+namespace DesktopManagerSample {
+    class Program {
+        static void Main(string[] args) {
+
+            Monitors monitor = new Monitors();
+            var test = monitor.GetMonitors();
+
+
+            Console.WriteLine("Available monitor connections: " + monitor.GetAvailableMonitorPaths());
+            Console.WriteLine("Connected monitors: " + monitor.GetConnectedMonitors().Count);
+
+            foreach (var deviceId in monitor.GetConnectedMonitors()) {
+                Console.WriteLine("==================================");
+                Console.WriteLine("MonitorID: " + deviceId);
+                Console.WriteLine("Wallpaper Path: " + monitor.GetWallpaper(deviceId));
+                Console.WriteLine("Wallpaper Position (only first monitor): " + monitor.GetWallpaperPosition());
+                var rect = monitor.GetMonitorRECT(deviceId);
+                Console.WriteLine("RECT: {0} {1} {2} {3}", rect.Left, rect.Top, rect.Right, rect.Bottom);
+            }
+
+            monitor.SetWallpaper(1, @"C:\Users\przemyslaw.klys\Downloads\IMG_4644.jpeg");
+        }
+    }
+}
