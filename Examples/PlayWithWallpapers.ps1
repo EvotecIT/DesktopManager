43a349759b2701e52e5bb81671d4f3a675e320b2
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-09-29 10:44:53 +0200

    Add whatif to examples

--- Examples/PlayWithWallpapers.ps1
+++ Examples/PlayWithWallpapers.ps1
@@ -4,5 +4,5 @@ Get-DesktopMonitors | Format-Table
 
 Get-DesktopWallpaper -Index 0
 
-Set-DesktopWallpaper -Index 1 -WallpaperPath "C:\Support\GitHub\ImagePlayground\Sources\ImagePlayground.Examples\bin\Debug\net7.0\Images\KulekWSluchawkach.jpg" -Position Fit
-Set-DesktopWallpaper -Index 0 -WallpaperPath "C:\Users\przemyslaw.klys\Downloads\IMG_4820.jpg"
+Set-DesktopWallpaper -Index 1 -WallpaperPath "C:\Support\GitHub\ImagePlayground\Sources\ImagePlayground.Examples\bin\Debug\net7.0\Images\KulekWSluchawkach.jpg" -Position Fit -WhatIf
+Set-DesktopWallpaper -Index 0 -WallpaperPath "C:\Users\przemyslaw.klys\Downloads\IMG_4820.jpg" -WhatIf
