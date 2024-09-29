a1c55f5c7887f9e00c3552b9b02e26bf7d23678d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-09-29 10:34:34 +0200

    Update parameters

--- Sources/DesktopManager.PowerShell/CmdletSetDesktopPosition.cs
+++ Sources/DesktopManager.PowerShell/CmdletSetDesktopPosition.cs
@@ -26,49 +26,43 @@ public sealed class CmdletSetDesktopPosition : PSCmdlet {
     /// <para type="description">The device ID of the monitor to set the position for.</para>
     /// </summary>
     [Alias("MonitorID")]
-    [Parameter(Mandatory = false, Position = 1, ParameterSetName = "Index")]
+    [Parameter(Mandatory = false, Position = 1, ParameterSetName = "DeviceID")]
     public string DeviceId;
 
     /// <summary>
     /// <para type="description">The device name of the monitor to set the position for.</para>
     /// </summary>
-    [Parameter(Mandatory = false, Position = 2, ParameterSetName = "Index")]
+    [Parameter(Mandatory = false, Position = 2, ParameterSetName = "DeviceName")]
     public string DeviceName;
-
-    /// <summary>
-    /// <para type="description">Set the position for connected monitors only.</para>
-    /// </summary>
-    [Parameter(Mandatory = false, Position = 3, ParameterSetName = "Index")]
-    public SwitchParameter ConnectedOnly;
 
     /// <summary>
     /// <para type="description">Set the position for the primary monitor only.</para>
     /// </summary>
-    [Parameter(Mandatory = false, Position = 4, ParameterSetName = "Index")]
+    [Parameter(Mandatory = false, Position = 3, ParameterSetName = "PrimaryOnly")]
     public SwitchParameter? PrimaryOnly;
 
     /// <summary>
     /// <para type="description">The left position of the monitor.</para>
     /// </summary>
-    [Parameter(Mandatory = true, Position = 5)]
+    [Parameter(Mandatory = true, Position = 4)]
     public int Left;
 
     /// <summary>
     /// <para type="description">The top position of the monitor.</para>
     /// </summary>
-    [Parameter(Mandatory = true, Position = 6)]
+    [Parameter(Mandatory = true, Position = 5)]
     public int Top;
 
     /// <summary>
     /// <para type="description">The right position of the monitor.</para>
     /// </summary>
-    [Parameter(Mandatory = true, Position = 7)]
+    [Parameter(Mandatory = true, Position = 6)]
     public int Right;
 
     /// <summary>
     /// <para type="description">The bottom position of the monitor.</para>
     /// </summary>
-    [Parameter(Mandatory = true, Position = 8)]
+    [Parameter(Mandatory = true, Position = 7)]
     public int Bottom;
 
     /// <summary>
@@ -78,14 +72,13 @@ public sealed class CmdletSetDesktopPosition : PSCmdlet {
         Monitors monitors = new Monitors();
 
         // Check if parameters are set by the user
-        bool? connectedOnly = MyInvocation.BoundParameters.ContainsKey(nameof(ConnectedOnly)) ? (bool?)ConnectedOnly : null;
         bool? primaryOnly = MyInvocation.BoundParameters.ContainsKey(nameof(PrimaryOnly)) ? (bool?)PrimaryOnly : null;
         int? index = MyInvocation.BoundParameters.ContainsKey(nameof(Index)) ? (int?)Index : null;
         string deviceId = MyInvocation.BoundParameters.ContainsKey(nameof(DeviceId)) ? DeviceId : null;
         string deviceName = MyInvocation.BoundParameters.ContainsKey(nameof(DeviceName)) ? DeviceName : null;
 
         // Get monitors
-        var getMonitors = monitors.GetMonitors(connectedOnly: connectedOnly, primaryOnly: primaryOnly, index: index, deviceId: deviceId, deviceName: deviceName);
+        var getMonitors = monitors.GetMonitors(connectedOnly: null, primaryOnly: primaryOnly, index: index, deviceId: deviceId, deviceName: deviceName);
         foreach (var monitor in getMonitors) {
             monitor.SetMonitorPosition(Left, Top, Right, Bottom);
         }
