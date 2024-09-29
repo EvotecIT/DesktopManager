c0def0660abef9408bd5d69fdcb79fafa06ec318
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-09-29 09:58:56 +0200

    Update examples

--- Examples/GetDesktopMonitors.ps1
+++ Examples/GetDesktopMonitors.ps1
@@ -9,8 +9,11 @@ $Desktop2 | Format-Table
 $Desktop3 = Get-DesktopMonitor -PrimaryOnly
 $Desktop3 | Format-Table
 
-$Desktop4 = Get-DesktopMonitor -Index 1
-$Desktop4 | Format-Table
+$Desktop4 = Get-DesktopMonitor -Index 0
+$Desktop4 | Format-Table *
 
-$Desktop5 = Get-DesktopMonitor -DeviceName "\\.\DISPLAY2"
-$Desktop5 | Format-Table
+$Desktop5 = Get-DesktopMonitor -Index 1
+$Desktop5 | Format-Table *
+
+$Desktop6 = Get-DesktopMonitor -DeviceName "\\.\DISPLAY2"
+$Desktop6 | Format-Table *
---
+++ Examples/SetDesktopMonitors.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module .\DesktopManager.psd1 -Force
+
+$Desktop2 = Get-DesktopMonitor -ConnectedOnly
+$Desktop2 | Format-Table
