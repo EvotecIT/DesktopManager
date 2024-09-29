ac21357660c3dfa13787599b431053e1c6546efa
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-09-29 10:34:48 +0200

    Update example

--- Examples/SetDesktopMonitors.ps1
+++ Examples/SetDesktopMonitors.ps1
@@ -2,3 +2,6 @@
 
 $Desktop2 = Get-DesktopMonitor -ConnectedOnly
 $Desktop2 | Format-Table
+
+Set-DesktopPosition -Index 0 -Left -3840 -Top 0 -Right 0 -Bottom 1660 -WhatIf
+Set-DesktopPosition -Index 1 -Left 0 -Top 0 -Right 3840 -Bottom 2160 -WhatIf
