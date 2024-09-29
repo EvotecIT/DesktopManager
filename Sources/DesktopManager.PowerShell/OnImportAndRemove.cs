5120a610f421d8dbb66abd095a1b8694eebd49e9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-09-29 09:24:16 +0200

    Add docs

--- Sources/DesktopManager.PowerShell/OnImportAndRemove.cs
+++ Sources/DesktopManager.PowerShell/OnImportAndRemove.cs
@@ -1,22 +1,36 @@
-using System;
 using System.IO;
-using System.Management.Automation;
 using System.Reflection;
-using System.Collections.Generic;
 
+/// <summary>
+/// OnModuleImportAndRemove is a class that implements the IModuleAssemblyInitializer and IModuleAssemblyCleanup interfaces.
+/// This class is used to handle the assembly resolve event when the module is imported and removed.
+/// </summary>
 public class OnModuleImportAndRemove : IModuleAssemblyInitializer, IModuleAssemblyCleanup {
+    /// <summary>
+    /// OnImport is called when the module is imported.
+    /// </summary>
     public void OnImport() {
         if (IsNetFramework()) {
             AppDomain.CurrentDomain.AssemblyResolve += MyResolveEventHandler;
         }
     }
 
+    /// <summary>
+    /// OnRemove is called when the module is removed.
+    /// </summary>
+    /// <param name="module"></param>
     public void OnRemove(PSModuleInfo module) {
         if (IsNetFramework()) {
             AppDomain.CurrentDomain.AssemblyResolve -= MyResolveEventHandler;
         }
     }
 
+    /// <summary>
+    /// MyResolveEventHandler is a method that handles the AssemblyResolve event.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
+    /// <returns></returns>
     private static Assembly MyResolveEventHandler(object sender, ResolveEventArgs args) {
         var libDirectory = Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location);
         var directoriesToSearch = new List<string> { libDirectory };
@@ -42,12 +56,23 @@ public class OnModuleImportAndRemove : IModuleAssemblyInitializer, IModuleAssemb
         return null;
     }
 
+    /// <summary>
+    /// Determine if the current runtime is .NET Framework
+    /// </summary>
+    /// <returns></returns>
     private bool IsNetFramework() {
         return System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET Framework", StringComparison.OrdinalIgnoreCase);
     }
+
+    // Determine if the current runtime is .NET Core
     private bool IsNetCore() {
         return System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET Core", StringComparison.OrdinalIgnoreCase);
     }
+
+    /// <summary>
+    /// Determine if the current runtime is .NET 5 or higher
+    /// </summary>
+    /// <returns></returns>
     private bool IsNet5OrHigher() {
         return System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET 5", StringComparison.OrdinalIgnoreCase) ||
                System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET 6", StringComparison.OrdinalIgnoreCase) ||
