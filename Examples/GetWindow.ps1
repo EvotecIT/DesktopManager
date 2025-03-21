0fac62622fbff915aa7314869cb9f5172b5b8429
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-03-21 19:26:00 +0100

    feat: ✨ Update `GetWindow.ps1` with additional `Send-DesktopText` examples
    * Added examples of `Send-DesktopText` for sending text to specific windows.
    * Commented out unused code for clarity.

--- Examples/GetWindow.ps1
+++ Examples/GetWindow.ps1
@@ -2,4 +2,11 @@
 
 Get-DesktopWindow | Format-Table *
 
-Set-DesktopWindow -Name '*Zadanie - Notepad' -Height 800 -Width 1200 -Left 100
+#Set-DesktopWindow -Name '*Zadanie - Notepad' -Height 800 -Width 1200 -Left 100
+
+
+#Get-Clipboard | Send-DesktopText -Name "Notepad"
+
+Send-DesktopText -Name "DesktopManager - GetWindow.ps1" -Text "Hello World" -Method Type -Verbose
+
+Send-DesktopText -Name "Remote Desktop Manager*" -Text "Hello World" -Method Type -Verbose
