357c634de518dba5ea909f57901ce5fd1e0f66cf
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-18 08:43:02 +0200

    feat: ✨ Update `GetDesktopScreenshot.ps1` to capture screenshots from multiple monitors
    * Modified the script to save screenshots for each monitor separately.
    * Added `-Monitor` parameter to specify the monitor for each screenshot.

--- Examples/GetDesktopScreenshot.ps1
+++ Examples/GetDesktopScreenshot.ps1
@@ -1,3 +1,6 @@
 ﻿Import-Module .\DesktopManager.psd1 -Force
 
-Get-DesktopScreenshot -Path "$PSScriptRoot\DesktopScreenshot.png"
+Get-DesktopScreenshot -Path "$PSScriptRoot\Output\DesktopScreenshot1.png" -Monitor 0
+Get-DesktopScreenshot -Path "$PSScriptRoot\Output\DesktopScreenshot2.png" -Monitor 1
+Get-DesktopScreenshot -Path "$PSScriptRoot\Output\DesktopScreenshot3.png" -Monitor 2
+Get-DesktopScreenshot -Path "$PSScriptRoot\Output\DesktopScreenshot4.png" -Monitor 3
