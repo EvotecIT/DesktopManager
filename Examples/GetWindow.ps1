54ebbbb3fb35e29c2057f4f44d9e0af667ee9149
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-18 10:21:41 +0200

    feat(GetWindow): ✨ Update Notepad window settings
    * Changed window name to '*Notepad' for consistency.
    * Added `-Activate` flag to set the window as active.
    * Included `-TopMost` option to keep the window on top.

--- Examples/GetWindow.ps1
+++ Examples/GetWindow.ps1
@@ -2,4 +2,6 @@
 
 Get-DesktopWindow | Format-Table *
 
-Set-DesktopWindow -Name '*Zadanie - Notepad' -Height 800 -Width 1200 -Left 100
+Set-DesktopWindow -Name '*Notepad' -Height 800 -Width 1200 -Left 100 -Activate
+
+Set-DesktopWindow -Name '*Notepad' -TopMost
