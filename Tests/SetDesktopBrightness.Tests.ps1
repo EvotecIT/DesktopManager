055c79cb378f5e0beb61a56645c38a833500bf15
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-19 13:49:13 +0200

    Revert "Implement monitor brightness support"

--- Tests/SetDesktopBrightness.Tests.ps1
+++
@@ -1,9 +0,0 @@
-BeforeAll {
-    Import-Module "$PSScriptRoot/../DesktopManager.psd1" -Force
-}
-
-describe 'Set-DesktopBrightness' {
-    it 'cmdlet is available' {
-        Get-Command Set-DesktopBrightness | Should -Not -BeNullOrEmpty
-    }
-}
