1fb2ffe4857ce885f5d094a23776b3e93e19a4fc
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-13 21:27:02 +0200

    fix(input): 🐛 ensure key up events for SendToControl

--- Sources/DesktopManager/KeyboardInputService.cs
+++ Sources/DesktopManager/KeyboardInputService.cs
@@ -96,9 +96,11 @@ public static class KeyboardInputService {
 
         foreach (VirtualKey key in keys) {
             MonitorNativeMethods.SendMessage(control.Handle, MonitorNativeMethods.WM_KEYDOWN, (uint)key, 0);
-            if ((key >= VirtualKey.VK_SPACE && key <= VirtualKey.VK_Z) || (key >= VirtualKey.VK_0 && key <= VirtualKey.VK_9)) {
+            if ((key >= VirtualKey.VK_SPACE && key <= VirtualKey.VK_Z) ||
+                (key >= VirtualKey.VK_0 && key <= VirtualKey.VK_9)) {
                 MonitorNativeMethods.SendMessage(control.Handle, MonitorNativeMethods.WM_CHAR, (uint)key, 0);
             }
+            MonitorNativeMethods.SendMessage(control.Handle, MonitorNativeMethods.WM_KEYUP, (uint)key, 0);
         }
     }
 }
