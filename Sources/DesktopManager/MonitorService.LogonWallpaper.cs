cff97efcbb9843d3f6dbd1d590fd113239fc7960
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-04-13 09:15:09 +0200

    Fallback to policy when WinRT lock screen APIs are missing

--- Sources/DesktopManager/MonitorService.LogonWallpaper.cs
+++ Sources/DesktopManager/MonitorService.LogonWallpaper.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 using Microsoft.Win32;
 #if NET8_0_OR_GREATER && WINDOWS && WINDOWS10_0_19041_0
@@ -40,55 +41,55 @@ public partial class MonitorService {
                     var file = StorageFile.GetFileFromPathAsync(imagePath).AsTask().GetAwaiter().GetResult();
                     LockScreen.SetImageFileAsync(file).AsTask().GetAwaiter().GetResult();
                     return;
-                } catch (InvalidOperationException) {
-                    throw;
                 } catch {
                     // ignore and use fallback
                 }
 #else
-                Type lockScreenType = Type.GetType(
-                    "Windows.System.UserProfile.LockScreen, Windows, ContentType=WindowsRuntime")
-                        ?? throw new InvalidOperationException("LockScreen type not found");
-                Type storageFileType = Type.GetType(
-                    "Windows.Storage.StorageFile, Windows, ContentType=WindowsRuntime")
-                        ?? throw new InvalidOperationException("StorageFile type not found");
+                if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                    throw new PlatformNotSupportedException("Logon wallpaper is only supported on Windows.");
+                }
 
-                var getFileMethod = storageFileType.GetMethod("GetFileFromPathAsync")
-                    ?? throw new InvalidOperationException("GetFileFromPathAsync method not found");
-                var fileOp = getFileMethod.Invoke(null, new object[] { imagePath });
-                if (fileOp == null) {
-                    throw new InvalidOperationException("GetFileFromPathAsync returned null");
+                Type? lockScreenType = Type.GetType(
+                    "Windows.System.UserProfile.LockScreen, Windows, ContentType=WindowsRuntime");
+                Type? storageFileType = Type.GetType(
+                    "Windows.Storage.StorageFile, Windows, ContentType=WindowsRuntime");
+
+                if (lockScreenType != null && storageFileType != null) {
+                    var getFileMethod = storageFileType.GetMethod("GetFileFromPathAsync")
+                        ?? throw new InvalidOperationException("GetFileFromPathAsync method not found");
+                    var fileOp = getFileMethod.Invoke(null, new object[] { imagePath });
+                    if (fileOp == null) {
+                        throw new InvalidOperationException("GetFileFromPathAsync returned null");
+                    }
+                    var asTaskMethod = fileOp.GetType().GetMethod("AsTask")
+                        ?? throw new InvalidOperationException("AsTask method not found on file operation");
+                    var fileTaskObject = asTaskMethod.Invoke(fileOp, null);
+                    if (fileTaskObject is not System.Threading.Tasks.Task fileTask) {
+                        throw new InvalidOperationException("AsTask did not return a Task for file operation");
+                    }
+                    fileTask.Wait();
+                    var fileProp = fileTask.GetType().GetProperty("Result")
+                        ?? throw new InvalidOperationException("Result property missing on task");
+                    var file = fileProp.GetValue(fileTask);
+                    if (file == null) {
+                        throw new InvalidOperationException("GetFileFromPathAsync returned a null result");
+                    }
+                    var setMethod = lockScreenType.GetMethod("SetImageFileAsync")
+                        ?? throw new InvalidOperationException("SetImageFileAsync method not found");
+                    var setOp = setMethod.Invoke(null, new object[] { file });
+                    if (setOp == null) {
+                        throw new InvalidOperationException("SetImageFileAsync returned null");
+                    }
+                    var opAsTaskMethod = setOp.GetType().GetMethod("AsTask")
+                        ?? throw new InvalidOperationException("AsTask method not found on set operation");
+                    var opTaskObject = opAsTaskMethod.Invoke(setOp, null);
+                    if (opTaskObject is not System.Threading.Tasks.Task opTask) {
+                        throw new InvalidOperationException("AsTask did not return a Task for set operation");
+                    }
+                    opTask.Wait();
+                    return;
                 }
-                var asTaskMethod = fileOp.GetType().GetMethod("AsTask")
-                    ?? throw new InvalidOperationException("AsTask method not found on file operation");
-                var fileTaskObject = asTaskMethod.Invoke(fileOp, null);
-                if (fileTaskObject is not System.Threading.Tasks.Task fileTask) {
-                    throw new InvalidOperationException("AsTask did not return a Task for file operation");
-                }
-                fileTask.Wait();
-                var fileProp = fileTask.GetType().GetProperty("Result")
-                    ?? throw new InvalidOperationException("Result property missing on task");
-                var file = fileProp.GetValue(fileTask);
-                if (file == null) {
-                    throw new InvalidOperationException("GetFileFromPathAsync returned a null result");
-                }
-                var setMethod = lockScreenType.GetMethod("SetImageFileAsync")
-                    ?? throw new InvalidOperationException("SetImageFileAsync method not found");
-                var setOp = setMethod.Invoke(null, new object[] { file });
-                if (setOp == null) {
-                    throw new InvalidOperationException("SetImageFileAsync returned null");
-                }
-                var opAsTaskMethod = setOp.GetType().GetMethod("AsTask")
-                    ?? throw new InvalidOperationException("AsTask method not found on set operation");
-                var opTaskObject = opAsTaskMethod.Invoke(setOp, null);
-                if (opTaskObject is not System.Threading.Tasks.Task opTask) {
-                    throw new InvalidOperationException("AsTask did not return a Task for set operation");
-                }
-                opTask.Wait();
-                return;
 #endif
-            } catch (InvalidOperationException) {
-                throw;
             } catch {
                 // ignore and use fallback
             }
@@ -127,36 +128,37 @@ public partial class MonitorService {
                 using FileStream fs = new FileStream(temp, FileMode.Create, FileAccess.Write);
                 stream.CopyTo(fs);
                 return temp;
-            } catch (InvalidOperationException) {
-                throw;
             } catch {
                 // ignore and use fallback
             }
 #else
-            Type lockScreenType = Type.GetType(
-                "Windows.System.UserProfile.LockScreen, Windows, ContentType=WindowsRuntime")
-                    ?? throw new InvalidOperationException("LockScreen type not found");
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                throw new PlatformNotSupportedException("Logon wallpaper is only supported on Windows.");
+            }
 
-            var getMethod = lockScreenType.GetMethod("GetImageStream")
-                ?? throw new InvalidOperationException("GetImageStream method not found");
-            var streamObj = getMethod.Invoke(null, null);
-            if (streamObj == null) {
-                throw new InvalidOperationException("GetImageStream returned null");
+            Type? lockScreenType = Type.GetType(
+                "Windows.System.UserProfile.LockScreen, Windows, ContentType=WindowsRuntime");
+
+            if (lockScreenType != null) {
+                var getMethod = lockScreenType.GetMethod("GetImageStream")
+                    ?? throw new InvalidOperationException("GetImageStream method not found");
+                var streamObj = getMethod.Invoke(null, null);
+                if (streamObj == null) {
+                    throw new InvalidOperationException("GetImageStream returned null");
+                }
+                var asStreamForRead = streamObj.GetType().GetMethod("AsStreamForRead")
+                    ?? throw new InvalidOperationException("AsStreamForRead method not found");
+                var streamObjResult = asStreamForRead.Invoke(streamObj, null);
+                if (streamObjResult is not Stream stream) {
+                    throw new InvalidOperationException("AsStreamForRead did not return a Stream");
+                }
+                using var streamReader = stream;
+                string temp = Path.GetTempFileName();
+                using FileStream fs = new FileStream(temp, FileMode.Create, FileAccess.Write);
+                streamReader.CopyTo(fs);
+                return temp;
             }
-            var asStreamForRead = streamObj.GetType().GetMethod("AsStreamForRead")
-                ?? throw new InvalidOperationException("AsStreamForRead method not found");
-            var streamObjResult = asStreamForRead.Invoke(streamObj, null);
-            if (streamObjResult is not Stream stream) {
-                throw new InvalidOperationException("AsStreamForRead did not return a Stream");
-            }
-            using var streamReader = stream;
-            string temp = Path.GetTempFileName();
-            using FileStream fs = new FileStream(temp, FileMode.Create, FileAccess.Write);
-            streamReader.CopyTo(fs);
-            return temp;
 #endif
-        } catch (InvalidOperationException) {
-            throw;
         } catch {
             // ignore and use fallback
         }
